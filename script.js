8ed25b38ca10dfd5060df10c41b26d7b8e12a60a
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2024-03-26 20:46:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const downloadButton = document.querySelector('.apk-selection button');
 apkSelect.addEventListener('change', function() {
     const selectedApk = this.value;
     let descriptionText = '';
-    let downloadLink; // nothing
+    let downloadLink; // No default link
 
     if (selectedApk === 'arceus') {
         descriptionText = "Arceus X, powered by SPDM team, capable of executing most scripts. (unable to make/send Webhooks, keep in mind.)";
@@ -13,12 +13,12 @@ apkSelect.addEventListener('change', function() {
     } else if (selectedApk === 'codex') {
         descriptionText = "Codex, also powered by SPDM team, simple and also capable of executing most scripts. (this APK cant make/send Webhooks unless your scripts code is modified yourself, keep in mind.)";
         downloadLink = 'https://www.mediafire.com/file/z72dvyk7kbac1v9/Codex_2.617.apk/file'; 
-    } else { // if selected option is "None"
+    } else { // Selected option is "None"
         descriptionText = "Pressing download will redirect you to the Arceus X download, trying to fix it rn.";
         downloadLink = '#'; // Prevent default download behavior 
     }
 
     apkDescription.textContent = descriptionText;
     downloadButton.href = downloadLink; 
-    downloadButton.target = '_blank'; 
+    downloadButton.target = '_blank'; // Open download links in a new tab
 });
