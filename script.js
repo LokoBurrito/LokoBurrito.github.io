b3a5891cd4c889cc85dc62ac4b18c5ebc31993a7
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2024-03-27 12:13:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// not useful for current site design
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Update button text
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = 'Dark Mode: ON';
+    } else {
+        darkModeToggle.textContent = 'Dark Mode: OFF';
+    }
+});
