048a6457848bc0e2e765529f2bd941e3a88f4773
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2024-03-27 13:21:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"> 
-const darkModeToggle = document.getElementById('darkModeToggle');
-
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-
-    // Update button text
-    if (document.body.classList.contains('dark-mode')) {
-        darkModeToggle.textContent = 'Dark Mode: ON';
-    } else {
-        darkModeToggle.textContent = 'Dark Mode: OFF';
-    }
-});
+// useles 
