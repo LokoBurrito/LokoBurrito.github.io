fbed6df9d3d059df9729be2b369e6f223ea6cb8c
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2024-03-27 19:00:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Get all the download buttons
-const downloadButtons = document.querySelectorAll('.download-btn');
 
-// Add event listeners to each button
-downloadButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent default form submission behavior
-
-        // Get the download link from the button's href attribute
-        const downloadLink = button.getAttribute('href');
-
-        // Redirect the user to the download link
-        window.location.href = downloadLink; 
-    });
-});
